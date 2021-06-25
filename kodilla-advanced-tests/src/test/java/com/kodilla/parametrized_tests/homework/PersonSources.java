ef691b52393f8f20ce8ec9eff607ee46e56bf5ad
Lukasz Wozniak <lukaszwozniak@outlook.com>
2021-06-25 12:34:02 +0200

    update siedem_trzy

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonSources.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonSources.java
@@ -8,12 +8,12 @@ public class PersonSources {
 
     static Stream<Arguments> provideDataForTestingBMI() {
         return Stream.of(
-                Arguments.of(new Person(1.76, 50.9)),
-                Arguments.of(new Person(1.55, 65.8)),
-                Arguments.of(new Person(1.90, 105.1)),
-                Arguments.of(new Person(1.62, 49.5)),
-                Arguments.of(new Person(1.93, 48.0)),
-                Arguments.of(new Person(2.10, 150.2))
+                Arguments.of(1.76, 50.9, "Underweight"),
+                Arguments.of(1.55, 65.8, "Overweight"),
+                Arguments.of(1.90, 105.1, "Overweight"),
+                Arguments.of(1.62, 49.5, "Normal (healthy weight)"),
+                Arguments.of(1.93, 48.0, "Very severely underweight"),
+                Arguments.of(2.10, 150.2, "Obese Class I (Moderately obese)")
         );
     }
 
--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
@@ -1,17 +1,13 @@
 package com.kodilla.parametrized_tests.homework;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTestSuite {
     @ParameterizedTest
     @MethodSource(value = "com.kodilla.parametrized_tests.homework.PersonSources#provideDataForTestingBMI")
-    void shouldCalculateBMIWithProvidedDataExternalClass(Person person) {
-        System.out.println(person.getBMI());
-        Stream<String> expected = Stream.of("Underweight","Obese Class V (Super Obese)","Overweight","Normal (healthy weight)","Very severely underweight","Obese Class I (Moderately obese)");
-        boolean check = expected.anyMatch( u-> u.equals(person.getBMI()));
-        assertTrue(check);
+    void shouldCalculateBMIWithProvidedDataExternalClass(double height, double weight, String expected) {
+        assertEquals(expected, new Person(height, weight).getBMI());
     }
 
 
