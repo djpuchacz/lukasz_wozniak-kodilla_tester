41d05d136fb21ccaecbcc08e97a5cc917c26ab2e
Lukasz Wozniak <lukaszwozniak@outlook.com>
2021-08-27 14:54:39 +0200

    15_3

--- kodilla-testcontainers/src/test/java/com/kodilla/testcontainers/ApplicationTest.java
+++ kodilla-testcontainers/src/test/java/com/kodilla/testcontainers/ApplicationTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.containers.DefaultRecordingFileFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.images.builder.ImageFromDockerfile;
@@ -16,6 +17,7 @@ import java.io.File;
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
+import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL;
 import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.SKIP;
 
 public class ApplicationTest {
@@ -28,10 +30,12 @@ public class ApplicationTest {
             new GenericContainer(
                     new ImageFromDockerfile()
                             .withFileFromClasspath("/tmp/index.html", "index.html")
+                            .withFileFromClasspath("/tmp/style.css", "style.css")
                             .withDockerfileFromBuilder(builder ->
                                     builder
                                             .from("httpd:2.4")
                                             .copy("/tmp/index.html", "/usr/local/apache2/htdocs")
+                                            .copy("/tmp/style.css", "/usr/local/apache2/htdocs")
                                             .build()))
                     .withNetwork(network)
                     .withNetworkAliases("my-server")
@@ -40,19 +44,22 @@ public class ApplicationTest {
     @Rule
     public BrowserWebDriverContainer chrome =
             new BrowserWebDriverContainer<>()
+                    .withCapabilities(new ChromeOptions())
                     .withNetwork(network)
-                    .withRecordingMode(SKIP, null)
-                    .withCapabilities(new ChromeOptions());
+                    .withRecordingMode(RECORD_ALL, new File("./build/"))
+                    // .withRecordingMode(SKIP, null)
+                    .withRecordingFileFactory(new DefaultRecordingFileFactory());
 
     @Test
     public void customImageTest() throws InterruptedException, IOException {
         RemoteWebDriver driver = chrome.getWebDriver();
         driver.get("http://my-server/");
 
+
         File screenshot = driver.getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(screenshot, new File("./build/screenshots/" + screenshot.getName()));
 
-        String title = driver.findElement(By.id("title")).getText();
-        assertEquals("My dockerized web page.", title);
+        String title = driver.findElement(By.xpath("/html/body/div/section[2]/h2[1]")).getText();
+        assertEquals("Moje hobby:", title);
     }
 }
